d079572b12b5b5f8ac687337ac7fb20da1535aab
vparedes15 <vaparedes15@gmail.com>
2020-03-24 00:27:35 -0500

    mas_fino_convierte_y_saluda

--- conversion.cpp
+++ conversion.cpp
@@ -5,6 +5,7 @@ int main (void)
   double millas=0;
   std::cout << "Hola, ingrese millas\n";
   std::cin  >> millas;
+  std::cout << "Eso en kilometros sería:\n";
   std::cout <<millas*1.609<< "\n";
   return 0;
 }
